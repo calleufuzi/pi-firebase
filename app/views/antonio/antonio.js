3e70ee506078bc22d009dda318d3de8194427d4e
podgorski <antonio@domob.me>
2018-11-01 18:22:32 -0300

    COMMIT PAGE

---
+++ app/views/antonio/antonio.js
@@ -0,0 +1,12 @@
+(function(){
+    // var firebase = require("firebase/app");
+    // require("firebase/auth");
+    
+    'use strict'
+    $(document).ready(function(){
+    
+        console.log('Tela de Login');
+        
+        
+    })    
+})()
